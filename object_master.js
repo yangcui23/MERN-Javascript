690bd36dbdf55b8f11edad45f1d8539ba980b31f
Yang Cui <123989119+yangcui23@users.noreply.github.com>
2023-04-03 00:42:17 -0600

    Update object_master.js

--- object_master.js
+++ object_master.js
@@ -50,33 +50,5 @@ console.log(flyingType);
 const normalType = pokémon.filter(p => p.types.includes("normal")).length;
 console.log(normalType);
 
-function ninjaBelt(ninja) {
-    return function belt(beltColor) { //note the closure here!
-        console.log("Ninja " + ninja + " has earned a " + beltColor + " belt.");
-    }
-}
-ninjaBelt('Eileen')('black'); //note the double invocation here.
-
-// here we have a function called "Outer"
-function outer() {
-    let count = 0; // this is a count variable that is scoped to the function
-    // there is an inner function that increments count and then console logs it
-    function inner() {
-        count++;
-        console.log(count);
-    }
-    // we're returning the inner function
-    return inner;
-}
-
-const counter = outer();   // counter is the function that we returned from calling the outer function
-counter();                 // this will console.log "1"
-counter();                 // this will console.log "2"
-counter();                 // this will console.log "3"
-counter();                 // this will console.log "4"
-
-// so that means that the count variable still exists! 
-// and it is being changed even though we aren't inside of the Outer function!
-// can we access count out here?
 
 
